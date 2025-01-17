0a6a4c42d2c4377c475ed91d6fc6166f1b432ee9
Chris Zak <kristof_evo@gmail.com>
2025-01-17 08:23:49 -0700

    Add project files.

---
+++ SayMyName/SayMyName.vb
@@ -0,0 +1,19 @@
+﻿Module SayMyName
+
+    Sub Main()
+        ' Prompt the user to enter their name
+        Console.Write("Please enter your name: ")
+
+        ' Save the user input to a variable
+        Dim userName As String = Console.ReadLine()
+
+        ' Display a hello message with the user's name
+        Console.WriteLine("Hello, " & userName & "!")
+
+        ' Prevent the console window from closing immediately
+        Console.WriteLine("Press any key to exit.")
+        Console.ReadKey()
+    End Sub
+
+End Module
+
